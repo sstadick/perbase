04fe243c97f330ffb2fc89c4a4d07f6fcd7fe8ef
yuanshenran <yuanshenran@yeah.net>
2022-05-01 21:25:37 +0800

    version upgrade

--- examples/par_granges_example.rs
+++ examples/par_granges_example.rs
@@ -45,7 +45,7 @@ impl<F: ReadFilter> RegionProcessor for BasicProcessor<F> {
     type P = PileupPosition;
 
     // This function receives an interval to examine.
-    fn process_region(&self, tid: u32, start: u32, stop: u32) -> Vec<Self::P> {
+    fn process_region(&self, tid: u32, start: u32, stop: u32, region_name: String) -> Vec<Self::P> {
         let mut reader = bam::IndexedReader::from_path(&self.bamfile).expect("Indexed reader");
         let header = reader.header().to_owned();
         // fetch the region
@@ -63,6 +63,7 @@ impl<F: ReadFilter> RegionProcessor for BasicProcessor<F> {
                         &header,
                         &self.read_filter,
                         None,
+                        region_name.parse().unwrap(),
                     ))
                 } else {
                     None
@@ -83,17 +84,17 @@ fn main() -> Result<()> {
 
     // Create the region processor
     let basic_processor = BasicProcessor {
-        bamfile: PathBuf::from("test/test.bam"),
+        bamfile: PathBuf::from("/home/yuanshenran/datasets/bam/gzy210418101_g_16_tumor_recal.bam"),
         read_filter: read_filter,
     };
 
     // Create a par_granges runner
     let par_granges_runner = par_granges::ParGranges::new(
-        PathBuf::from("test/test.bam"),       // pass in bam
-        None,                                 // optional ref fasta
+        PathBuf::from("/home/yuanshenran/datasets/bam/gzy210418101_g_16_tumor_recal.bam"),  // pass in bam
+        Some(PathBuf::from("/yunying/ref/human/b37/b37_Homo_sapiens_assembly19.fasta")),   // optional ref fasta
+        Some(PathBuf::from("/home/yuanshenran/datasets/bam/complex/egfr_exon20_insertion.bed")), // bedfile to narrow regions
         None, // optional bcf/vcf file to specify positions of interest
-        Some(PathBuf::from("test/test.bed")), // bedfile to narrow regions
-        true, // Merge any overlapping regions in the BED file
+        true, // merge overlap in bed or vcf
         None, // optional allowed number of threads, defaults to max
         None, // optional chunksize modification
         None, // optional modifier on the size of the channel for sending Positions
@@ -103,7 +104,8 @@ fn main() -> Result<()> {
     // Run the processor
     let receiver = par_granges_runner.process()?;
     // Pull the in-order results from the receiver channel
-    receiver.into_iter().for_each(|p: PileupPosition| {
+    receiver.into_iter().for_each(|mut p: PileupPosition| {
+        p.update_insert_statistic();
         // Note that the returned values are required to be `serde::Serialize`, so more fancy things
         // than just debug printing are doable.
         println!("{:?}", p);
